5276a2280dc2babd2705399ef497d11875d5bc80
David Camperos <davidapks@gmail.com>
2020-05-06 20:23:44 -0400

    Basic header added

---
+++ src/components/header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>TODOLIST</h1>
+            <nav>
+                <ul>
+                    <li>INFO</li>
+                    <p>&nbsp;&nbsp;|&nbsp;&nbsp;</p>
+                    <li>APP</li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+  
+
+export default Header;
