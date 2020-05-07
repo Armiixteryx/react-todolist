57873371cbbf8441585272ebac7ddf8ba5b636db
David Camperos <davidapks@gmail.com>
2020-05-07 19:50:26 -0400

    Basic task adder

---
+++ src/components/Todolist/TaskAdder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import styled from 'styled-components';
+
+const TaskAdder = () => {
+    return (
+        <div>
+            <input placeholder="ENTER A TASK TO BE DONE"></input>
+            <button></button>
+        </div>
+    );
+}
+
+export default TaskAdder;
